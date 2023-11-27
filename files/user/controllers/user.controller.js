413f3d10bbf7a7af98e1e777a85fa76d09921426
codewithemmy <codewithemmy@gmail.com>
2023-11-27 22:13:45 +0100

    update

--- files/user/controllers/user.controller.js
+++ files/user/controllers/user.controller.js
@@ -16,7 +16,7 @@ const createUserController = async (req, res, next) => {
 
 const userLoginController = async (req, res, next) => {
   const [error, data] = await manageAsyncOps(UserService.userLogin(req.body))
-console.log("error", error)
+
   if (error) return next(error)
 
   if (!data.success) return next(new CustomError(data.msg, BAD_REQUEST, data))
