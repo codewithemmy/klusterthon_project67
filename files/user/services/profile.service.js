26681abe1194686417c2aab40c23cbf912e03d56
codewithemmy <codewithemmy@gmail.com>
2023-12-01 00:09:03 +0100

    udpate

--- files/user/services/profile.service.js
+++ files/user/services/profile.service.js
@@ -122,9 +122,7 @@ class ProfileService {
   }
 
   static async getUserProfileService(payload) {
-    const { _id } = payload
-    if (_id === undefined || payload === undefined)
-      return { success: true, msg: UserFailure.FETCH, data: null }
+ 
     const user = await UserRepository.findSingleUserWithParams(
       {
         ...payload,
